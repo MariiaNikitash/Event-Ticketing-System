485cbc9d7706255defff2569b3dbb808fde2fab5
kalmk <juyeongk365@csu.fullerton.edu>
2026-03-03 21:11:51 -0800

    Update BookingRequestDTO.java

--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/dto/BookingRequestDTO.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/dto/BookingRequestDTO.java
@@ -1,8 +1,10 @@
 package com.example.event_ticketing_system.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class BookingRequestDTO {
     private Integer attendeeId;
     private Integer ticketTypeId;
