4e7815b56d517b3779b26bb4f3cb86151b53c20d
Ju Yeong Kim <111930167+kalmk@users.noreply.github.com>
2026-03-04 15:30:04 -0800

    push it (NO EROR)

--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/repository/BookingRepository.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/repository/BookingRepository.java
@@ -1,8 +1,6 @@
 package com.example.event_ticketing_system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.event_ticketing_system.entity.Booking;
@@ -10,12 +8,4 @@ import com.example.event_ticketing_system.entity.Booking;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
-  @Query("""
-          SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END
-          FROM Booking b
-          WHERE b.attendee.attendeeId = :attendeeId
-            AND b.ticketType.ticketTypeId = :ticketTypeId
-      """)
-  boolean alreadyBooked(@Param("attendeeId") Integer attendeeId,
-      @Param("ticketTypeId") Integer ticketTypeId);
 }
