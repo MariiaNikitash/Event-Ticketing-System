05766fe26d964ec6a8540293b6ce4c67f542f206
Ju Yeong Kim <111930167+kalmk@users.noreply.github.com>
2026-03-04 14:52:56 -0800

    Revert "fix"
    This reverts commit 52fd377c2b1d68791bc680245abc2debfd18655c.

--- event-ticketing-system/src/main/java/com/example/event_ticketing_system/repository/VenueRepository.java
+++ event-ticketing-system/src/main/java/com/example/event_ticketing_system/repository/VenueRepository.java
@@ -1,11 +1,9 @@
 package com.example.event_ticketing_system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.event_ticketing_system.entity.Venue;
 
-@Repository
 public interface VenueRepository extends JpaRepository<Venue, Integer> {
     
 }
